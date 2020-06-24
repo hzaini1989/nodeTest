2fe354289ee1086e078d38e1d2345ae14d615ae7
huangzhao <huangzhao@missfresh.cn>
2020-06-24 10:46:54 +0800

    feat:exports 变量

---
+++ commonjs/index.js
@@ -0,0 +1,3 @@
+console.log('start...')
+var lib = require('./lib.js')
+console.log('end...',lib)
---
+++ commonjs/lib.js
@@ -0,0 +1,11 @@
+console.log('hello lib.js')
+
+exports.hello = 'world'
+
+exports.add = function(a,b){
+  return a + b
+}
+
+exports.geekbang = {
+  hello:'world'
+}
